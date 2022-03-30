7fb5b85d558e9c64961fc6a9f09e879b928661d0
shinrinG <h06.kojima@gmail.com>
2022-03-31 02:23:06 +0900

    InCreate DCTExample

--- EigHandle/EigHandle/EigHandle.cpp
+++ EigHandle/EigHandle/EigHandle.cpp
@@ -1,71 +1,68 @@
 ﻿#include <iostream>
+#include <string>
 #include "defs.h"
 #include "Eigen/Core"
 
-bool EigFunc(Eigen::MatrixXf& dst,const Eigen::MatrixXf& src)
-{
-    bool bret = true;
-    
-
-
-    return bret;
-}
-
-bool Arr2EMat() 
-{
-    bool bret = true;
-
-    return bret;
-}
-
-bool EMat2Arr() 
-{
-    bool bret = true;
-
-    return bret;
-}
-
-bool EMatOperate(Eigen::MatrixXf& dst, const Eigen::MatrixXf& src)
-{
-    bool bret = true;
-    bret = (src.cols() == 2) && (dst.cols() == 2);
-    if (bret) 
-    {
-        Eigen::MatrixXf base = Eigen::MatrixXf::Zero(2, 1);
-        base(0, 0) = 1;
-        base(1, 0) = 1;
-        dst = src * base;
-    }
-    return bret;
-}
-
-
-//MainProc
-int main()
+//HandlingEigenMatrix
+void Sample1_EMatHandle()
 {
     //RawData
     unsigned char raw_size = 4;
     size_t mem_size = raw_size * sizeof(float);
     float* src = new float[raw_size];
+    float* dst = new float[raw_size];
     memset(src, 0, mem_size);
+    memset(dst, 0, mem_size);
     float cur = 1.0f;
     for (size_t i = 0; i < raw_size; i++)
     {
         src[i] = cur;
-        cur+=1.0;
+        cur += 1.0;
     }
-    std::cout << src[3] << "\n";
+
 
     //HandleData
-    EMatXf e_src = EMatXf::Zero(2,2);
+    EMatXf e_src = EMatXf::Zero(2, 2);
     EMatXf e_dst = EMatXf::Zero(2, 2);
 
-    
-    std::cout << e_src;
+    std::cout << "InitialMat" << std::endl;
+    std::cout << e_src << std::endl;
+
+    Arr2EMat(e_src, src, raw_size);
+
+    std::cout << "UpdatedMat" << std::endl;
+    std::cout << e_src << std::endl;
+
+    EMatOperate(e_dst, e_src);
+
+    std::cout << "ProcedMat" << std::endl;
+    std::cout << e_dst << std::endl;
+
+    EMat2Arr(dst, raw_size, e_src);
+
+    std::cout << "ResArr" << std::endl;
+    for (size_t i = 0; i < raw_size; i++)
+    {
+        std::cout << dst[i] << ",";
+    }
+    std::cout << std::endl;
 
     //Mem Release
-    e_src.resize(0,0);
+    e_src.resize(0, 0);
     e_dst.resize(0, 0);
     delete[] src;
+    delete[] dst;
+}
+
+//Test 2D-UnitalyDCT for SquareInput
+void Sample2_Uni2DDCT_Sq()
+{
+    
+}
+
+//MainProc
+int main()
+{
+    Sample1_EMatHandle();
     return 0;
 }
--- EigHandle/EigHandle/defs.h
+++ EigHandle/EigHandle/defs.h
@@ -1,4 +1,172 @@
 #pragma once
 #include "Eigen/Core"
+#include <vector>
+#include <string>
+#include "math.h"
 
 typedef Eigen::MatrixXf EMatXf;
+typedef Eigen::VectorXf EVecXf;
+
+bool Arr2EMat(
+    EMatXf& dst,
+    const float* const src,
+    const size_t src_size
+)
+{
+    bool bret = true;
+    bool bExec = false;
+    size_t rows = dst.rows();
+    size_t cols = dst.cols();
+    bExec = (rows * cols >= src_size) ? true : false;
+
+    if (bExec)
+    {
+        size_t cur_num = 0;
+        for (size_t c = 0; c < cols; c++)
+        {
+            for (size_t r = 0; r < rows; r++)
+            {
+                cur_num = c * rows + r;
+                dst(r, c) = (cur_num < src_size) ? src[cur_num] : 0.0f;
+            }
+        }
+    }
+    else
+    {
+        bret = false;
+    }
+
+    return bret;
+}
+
+bool EMat2Arr(
+    float* dst,
+    size_t dst_size,
+    const EMatXf& src
+)
+{
+    bool bret = true;
+    bool bExec = false;
+    size_t rows = src.rows();
+    size_t cols = src.cols();
+    bExec = (rows * cols == dst_size) ? true : false;
+    if (bExec)
+    {
+        size_t cur_num = 0;
+        for (size_t c = 0; c < cols; c++)
+        {
+            for (size_t r = 0; r < rows; r++)
+            {
+                cur_num = c * rows + r;
+                dst[cur_num] = src(r, c);
+            }
+        }
+    }
+    else
+    {
+        bret = false;
+    }
+    return bret;
+}
+
+bool EMatOperate(Eigen::MatrixXf& dst, const Eigen::MatrixXf& src)
+{
+    bool bret = true;
+    bret = (src.cols() == 2) && (dst.cols() == 2);
+    if (bret)
+    {
+        Eigen::MatrixXf base = Eigen::MatrixXf::Zero(2, 1);
+        base(0, 0) = 1;
+        base(1, 0) = 1;
+        dst = src * base;
+    }
+    return bret;
+}
+
+/// <summary>
+/// Dictionary of 2D-DCT Only Sqare
+/// </summary>
+class DCTSqDictionary{
+public:
+    size_t m_size;
+    std::vector<EMatXf> m_vterm;// Vector of BasisImg
+    std::vector<EVecXf> m_vbasis;// Vector of Basis
+
+    DCTSqDictionary():
+        m_size(1),
+        m_vterm(0),
+        m_vbasis(0)
+    {
+    }
+
+    DCTSqDictionary(size_t sqsize):
+        m_size(sqsize*sqsize),
+        m_vterm(0)
+    {
+        const double pi = 3.141592653589793238463;
+        // Dim^4
+        for(int kx =0;kx<m_size;kx++)
+        {
+            for (int ky = 0; ky < m_size; ky++)
+            {
+                //Gen 1 Basis
+                EMatXf cur_img(m_size, m_size);
+                for (int c = 0; c < m_size; c++)
+                {
+                    for (int r = 0; r < m_size; r++)
+                    {
+                        cur_img(r, c) = (float)(std::cos((2 * c + 1) * kx * pi / (2 * m_size)) * std::cos((2 * r + 1) * ky * pi / (2 * m_size)));
+                    }
+                }
+                m_vterm.push_back(cur_img);
+            }
+
+        }
+        
+    }
+
+    ~DCTSqDictionary()
+    {
+        m_vterm.clear();
+        m_vbasis.clear();
+    }
+
+    //TODO
+    bool genBasis()
+    {
+        return false;
+    }
+
+
+};
+
+/// <summary>
+/// 2D-UnitaryDCT Only Sqare
+/// </summary>
+/// <param name="dst"></param>
+/// <param name="src"></param>
+/// <returns></returns>
+bool UnitaryDCT2DSq(EMatXf& dst,const EMatXf& src)
+{
+    bool bret = true;
+    bool bExec = false;
+    size_t rows = src.rows();
+    size_t cols = src.cols();
+    if((rows==dst.rows())&&(cols==dst.cols())&&(cols==rows))
+    {
+        bExec = true;
+    }
+
+    if(bExec)
+    {
+        EMatXf e_conv = Eigen::MatrixXf::Zero(rows,cols);
+        //TODO
+
+
+
+        dst = e_conv * src * e_conv.transpose(); //UnitaryTransform for Signal Matrix(Z = RXR^T)
+    }
+    
+
+    return bret;
+}
